9b7a814279e23a83fbad2b88fcc06a3819a4417c
Ricardo <Ricardo.Herrera@zemog.com.mx>
2020-07-17 04:47:07 -0600

    Se modifico el parametro de localhost

--- includes/parametros.php
+++ includes/parametros.php
@@ -1,8 +1,8 @@
 <?php 
 //Telmex
 //define('url', 'http://189.248.106.65/TransportadoraZemog/');
-// url para servidor define('url', 'http://transportezemog.hopto.org/TransportadoraZemog/');
-define('url', 'http://localhost/TRANSPORTADORA_ZEMOG-pro/');
+//url para servidor define('url', 'http://transportezemog.hopto.org/TransportadoraZemog/');
+//define('url', 'http://localhost/TRANSPORTADORA_ZEMOG-pro/');
 
 
 //define('url', 'http://transportezemog.hopto.org/TransportadoraZemog/');
@@ -10,7 +10,7 @@ define('url', 'http://localhost/TRANSPORTADORA_ZEMOG-pro/');
 //define('url', 'http://187.189.71.128/TransportadoraZemog/');
 
 
-//define('url', 'http://localhost//TransportadoraZemog/');
+define('url', 'http://localhost/');
 define('Empresa', 'Transportadora Zemog');
 
 
